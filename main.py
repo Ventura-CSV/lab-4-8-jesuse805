e8c8cbe5dddae20df6938783edf93e7c60ed7cf9
jesus <jesus_estrada5@my.vcccd.edu>
2024-06-28 14:28:23 -0700

    CS V11

--- main.py
+++ main.py
@@ -1,23 +1,18 @@
 def main():
     
-    while True:
-        try:
-            inputInteger1 = int(input(''))
-            inputInteger2 = int(input(''))
-            
-            if inputInteger1 > 1 and inputInteger2 > inputInteger1:
-                break
-            else:
-                print('Invalid')
-        except ValueError:
-            
-            
-    
-    
-    
-    
-    
     plist = []
+    
+    inputInt1 = int(input(''))
+    inputInt2 = int(input(''))
+            
+    if inputInt1 <= 1 or inputInt2 <= 1 or inputInt2 < inputInt1:
+        print('Value Error')
+    else:
+        for i in range(inputInt1, inputInt2 + 1):
+            countValue = 0
+            for j in range(2, i):
+            
+    
     
     return plist
 
