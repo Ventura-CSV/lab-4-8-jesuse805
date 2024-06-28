dd12fb24d51839cdb89e4cb8bf6d1da6f876de9f
jesus <jesus_estrada5@my.vcccd.edu>
2024-06-28 14:33:23 -0700

    CS V11

--- main.py
+++ main.py
@@ -2,15 +2,21 @@ def main():
     
     plist = []
     
-    inputInt1 = int(input(''))
-    inputInt2 = int(input(''))
+    inputInteger1 = int(input(''))
+    inputInteger2 = int(input(''))
             
-    if inputInt1 <= 1 or inputInt2 <= 1 or inputInt2 < inputInt1:
+    if inputInteger1 <= 1 or inputInteger2 <= 1 or inputInteger2 < inputInteger1:
         print('Value Error')
     else:
-        for i in range(inputInt1, inputInt2 + 1):
+        for i in range(inputInteger1, inputInteger2 + 1):
             countValue = 0
             for j in range(2, i):
+                if i % j == 0:
+                    countValue = countValue + 1
+                break
+        if countValue == 0:
+            plist.append(i)
+    print(plist)
             
     
     
